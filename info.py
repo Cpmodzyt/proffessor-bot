b1d1e1d316ba2c67ed0f2e8b7ce1a99a78861aa5
P A M O D <59545375+pamodmadubashana@users.noreply.github.com>
2024-10-27 01:10:29 +0530

    Update info.py

--- info.py
+++ info.py
@@ -11,13 +11,13 @@ def is_enabled(value, default):
 
 
 # PyroClient Setup 
-API_ID = int(environ['25812247'])
-API_HASH = environ['00a50dc03d5b63a0eabfca83fa02c4ca']
-BOT_TOKEN = environ['7876241546:AAGVoSPiHeTAJu14J1Xpe-mmAvkCBMURlys']
+API_ID = 25812247
+API_HASH = '00a50dc03d5b63a0eabfca83fa02c4ca'
+BOT_TOKEN = '7876241546:AAGVoSPiHeTAJu14J1Xpe-mmAvkCBMURlys'
 
 # Bot settings
 WEB_SUPPORT = bool(environ.get("WEBHOOK", 'True')) # for web support on/off
-PICS = (environ.get('PICS' ,'https://graph.org/file/01ddfcb1e8203879a63d7.jpg https://graph.org/file/d69995d9846fd4ad632b8.jpg https://graph.org/file/a125497b6b85a1d774394.jpg https://graph.org/file/43d26c54d37f4afb830f7.jpg https://graph.org/file/60c1adffc7cc2015f771c.jpg https://graph.org/file/d7b520240b00b7f083a24.jpg https://graph.org/file/0f336b0402db3f2a20037.jpg https://graph.org/file/39cc4e15cad4519d8e932.jpg https://graph.org/file/d59a1108b1ed1c6c6c144.jpg https://te.legra.ph/file/3a4a79f8d5955e64cbb8e.jpg https://graph.org/file/d69995d9846fd4ad632b8.jpg')).split()
+PICS = ['https://graph.org/file/01ddfcb1e8203879a63d7.jpg https://graph.org/file/d69995d9846fd4ad632b8.jpg https://graph.org/file/a125497b6b85a1d774394.jpg https://graph.org/file/43d26c54d37f4afb830f7.jpg https://graph.org/file/60c1adffc7cc2015f771c.jpg https://graph.org/file/d7b520240b00b7f083a24.jpg https://graph.org/file/0f336b0402db3f2a20037.jpg https://graph.org/file/39cc4e15cad4519d8e932.jpg https://graph.org/file/d59a1108b1ed1c6c6c144.jpg https://te.legra.ph/file/3a4a79f8d5955e64cbb8e.jpg https://graph.org/file/d69995d9846fd4ad632b8.jpg']
 UPTIME = time.time()
 
 # Admins, Channels & Users
